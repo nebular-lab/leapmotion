65f99b4ee63e7920752dea0fc567e12835939f2c
hiranodaisuke <hiranodaisuke0721@gmail.com>
2021-06-06 18:21:16 +0900

    add new file

---
+++ LEAP/LeapSDK/docs/LeapC/navtreedata.js
@@ -0,0 +1,25 @@
+var NAVTREE =
+[
+  [ "Leap Motion C API", "index.html", [
+    [ "LeapC Guide", "index.html", "index" ],
+    [ "API Reference", "modules.html", "modules" ],
+    [ "Examples", "examples.html", "examples" ],
+    [ "Release Notes", "md_Leap_SDK_Release_Notes.html", "md_Leap_SDK_Release_Notes" ],
+    [ "Using the Diagnostic Visualizer", "visualizer.html", null ],
+    [ "Using the Leap Motion Control Panel", "control_panel.html", null ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Field Index", "functions.html", [
+      [ "All", "functions.html", null ],
+      [ "Variables", "functions_vars.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"callback-example.html",
+"group___structs.html#a2bf4f1f4fdc60bf1ddfda6905b626840"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
